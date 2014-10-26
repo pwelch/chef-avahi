6fbf164965e105f17b5e81c014871f29e45be73d
pwelch <paul@pwelch.net>
2014-10-26 16:22:15 -0400

    Update Dependencies
    - Update Berkshelf and ChefSpec
    - Update ChefSpec test to fix Deprecation Warning
    - Bump Cookbook Version for changes
    - Remove .ruby-version file and use ruby version in Gemfil

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'avahi::default' do
-  let(:chef_run)               { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run)               { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it "should install the avahi-daemon package" do
     expect(chef_run).to install_package 'avahi-daemon'
