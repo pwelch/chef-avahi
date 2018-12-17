1b59e20ce67c209ec4d5057311bf460e2a6bb04b
Stephen Hoekstra <shoekstra@schubergphilis.com>
2018-12-17 13:41:00 +0100

    Fix to make avahi work in docker
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,3 +19,13 @@ default['avahi']['hostname'] = node['hostname']
 default['avahi']['domain']   = 'local'
 default['avahi']['useipv4']  = 'yes'
 default['avahi']['useipv6']  = 'no'
+
+# rlimits config
+
+default['avahi']['rlimit']['conf'] = true
+default['avahi']['rlimit']['core'] = 0
+default['avahi']['rlimit']['data'] = 4194304
+default['avahi']['rlimit']['fsize'] = 0
+default['avahi']['rlimit']['nofile'] = 300
+default['avahi']['rlimit']['nproc'] = 3
+default['avahi']['rlimit']['stack'] = 4194304
